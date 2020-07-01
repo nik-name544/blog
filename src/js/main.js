fdfb5fe33c45e082c0bf546482d2773f268f8d2a
nik-name544 <revenkonik30@gmail.com>
2020-07-01 18:50:30 +0300

    questions-upd_v0.4

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
 $(function () {
 
+    // var qBtn = $('.questions__item-btn');
+
+    // qBtn.on('click', function () {
+    //     qBtn.removeClass('on');
+    //     $(this).addClass('on');
+    // });
+
+    // $('.questions__item-btn').on('click', function () {
+    //     $('.questions__item-answer').removeClass('on');
+    //     $(this).toggleClass("on");
+    // });
+
+    $('.questions__item').on('click', function () {
+        $(this).toggleClass("on");
+    });
+
+    // $('.questions__item').click(function (event) {
+    //     // $('.on').removeClass('on');
+    //     $(this).toggleClass('on');
+    // });
 });
