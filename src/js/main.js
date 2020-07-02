fe09b13e3eaef4e6fac5add905aa12debad29404
nik-name544 <revenkonik30@gmail.com>
2020-07-03 00:05:06 +0300

    adaptive-upd_v1

--- src/js/main.js
+++ src/js/main.js
@@ -16,6 +16,16 @@ $(function () {
         $(this).toggleClass("on");
     });
 
+    $('.header__btn').on('click', function () {
+        $('.header__menu-list').toggleClass("block");
+        $('.header__menu').toggleClass("block");
+        $('.header__logo-inner').toggleClass("block");
+    });
+
+
+
+
+
     // $('.questions__item').click(function (event) {
     //     // $('.on').removeClass('on');
     //     $(this).toggleClass('on');
